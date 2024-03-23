b03b67408c9a4cce3f921bb33d1b24d4ebdd89a8
aogie <nguyenhoang3519@gmail.com>
2024-03-23 15:11:42 +0700

    create Student

--- src/main/java/com/gdu/demo/Student.java
+++ src/main/java/com/gdu/demo/Student.java
@@ -1,4 +1,13 @@
 package com.gdu.demo;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
 public class Student {
+    private String mssv;
+    private String name;
+    private String email;
+    private String className;
 }
--- src/main/java/com/gdu/demo/StudentController.java
+++ src/main/java/com/gdu/demo/StudentController.java
@@ -1,4 +1,18 @@
 package com.gdu.demo;
 
-public class StudentController g{
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/student")
+public class StudentController {
+    @GetMapping
+    public String getStudent() {
+        Student student = new Student(
+                "2104110066",
+
+        );
+        return "Hello Student";
+    }
 }
